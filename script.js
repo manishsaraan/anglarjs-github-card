72e6079f8f113445d0e29b7c3c466ed150d451ac
manishsaraan <manish021js@gmail.com>
2017-11-12 16:14:16 +0530

    gamification search

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Code goes here
 (function() {
   var app = angular.module('myApp', []);
-      app.controller('MainController',["$http","$scope", function($http, $scope) {
+      app.controller('MainController',["$http","$scope", "$interval", function($http, $scope, $interval) {
       $scope.message = "Github Viewer!";
       $scope.isError = false;
       $scope.repoSortOrder = "-stargazers_count";
+      $scope.countdown = 10;   
       var onUserComplete = function(res) {
         $scope.isError = false;
         $scope.user = res.data;      
@@ -15,6 +16,19 @@
       var onRepos = function(res){
          $scope.repos = res.data;
       }
+
+      var decrementCountdown = function(){
+         $scope.countdown -= 1;
+         if($scope.countdown < 1){
+           $scope.search($scope.username);
+         }
+      };
+
+      var startCountdown = function(){
+         $interval(decrementCountdown, 1000, $scope.countdown);
+      };
+      startCountdown();
+
       var onError = function() {
         $scope.isError = true;
         $scope.error = "could not fetch the data";
@@ -24,6 +38,6 @@
                $http.get("https://api.github.com/users/"+username)
                     .then(onUserComplete, onError);
       };
-
+      
     }]);
 })();
